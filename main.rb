046b57f9576a6ac77a877e5edaa63267469777c3
Ruslan Zaytsev <breezzz@gmail.com>
2017-10-15 00:23:18 +0300

    screen prototype

--- main.rb
+++ main.rb
@@ -1,14 +1,40 @@
 require_relative './lib/hand'
 
+def screen(user, dealer, open = false)
+  system('clear')
+  puts open.class
+  puts '#-------------------#'
+  puts 'YOUR CARDS:'
+  user.cards.map {|card| print "#{card.name}\t"}
+  puts ''
+  puts "Очков   #{user.check_points}"
+  puts "Кошелек   #{user.money}"
+  puts '#-------------------#'
+  puts 'Dealer CARDS:'
+  case open
+    when true
+      dealer.cards.map {|card| print "#{card.name}\t"}
+      puts ''
+      puts "Очков   #{dealer.check_points}"
+    else
+      dealer.cards.map {|card| print "*\t"}
+      puts ''
+  end
+  puts "Кошелек   #{dealer.money}"
+  puts '#-------------------#'
+end
+
 master = Hand.new('Ruslan')
 dealer = Hand.new('JoJo', false)
 deck = Deck.new
 deck.shuffle
 loop do
   master.get_card(deck.take_card)
-  puts master.cards.inspect
-  puts "очков   #{master.check_points}"
+  dealer.get_card(deck.take_card)
+  screen(master, dealer, true)
+#  puts master.cards.inspect
+#  puts "очков   #{master.check_points}"
   break if gets.chomp == 'qwe'
-  puts '#---------------------------------#'
+#  puts '#---------------------------------#'
 end
 
